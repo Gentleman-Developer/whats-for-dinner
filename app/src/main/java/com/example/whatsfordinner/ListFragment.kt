420da71a2ae6938337cf4cde8e789f7a9405c73f
Gentleman <gentlemandeveloper@outlook.com>
2025-09-04 21:05:52 +0300

    fragments added to the project

---
+++ app/src/main/java/com/example/whatsfordinner/ListFragment.kt
@@ -0,0 +1,31 @@
+package com.example.whatsfordinner
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.whatsfordinner.databinding.FragmentListBinding
+
+
+class ListFragment : Fragment() {
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+
+}
